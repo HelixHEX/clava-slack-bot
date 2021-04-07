30a79a6d3b8bbc190f55c587c7f15a9c785de85b
HelixHEX <e.wambugu192@gmail.com>
2021-04-07 15:01:55 -0400

    Added express back

--- src/server.js
+++ src/server.js
@@ -1,78 +1,39 @@
-// import 'dotenv-safe/config'
-// import "reflect-metadata";
-
-// import express from "express";
-
-// const cors = require("cors");
-// const morgan = require('morgan')
-
-// // const message = require('./routes/message')
-
-// import { client, socketModeClient } from './utils/slack'
-
-// import cron from "cron";
-// import fetch from 'node-fetch'
-
-// const main = () => {
-//   const app = express();
-
-//   app.use(morgan("dev"));
-
-//   app.use(cors({ origin: "*" }));
-
-//   app.use(express.json());
-//   app.use(express.urlencoded({ extended: true }));
-
-//   app.get("/", (_, res) => {
-//     res.send("Hello world");
-//   });
-
-//   // app.use('/api/v1/message', message)
-
-//   app.use((_, res) => {
-//     res.status(404).json({ status: "404" });
-//   });
-
-//   //listen for messages 
-//   socketModeClient.on('message', async ({ event }) => {
-//     await client.reactions.add({
-//       timestamp: event.ts,
-//       name: 'clava',
-//       channel: event.channel
-//     })
-//     console.log('Reaction added')
-//   });
-
-//   //start listening for messages
-//   (async () => {
-//     await socketModeClient.start();
-//   })();
-
-//   //prevent heroku from sleeping
-//   const cronJob = new cron.CronJob("0 */25 * * * *", () => {
-//     fetch("https://clava-slack-app.herokuapp.com/")
-//       .then((res) =>
-//         console.log(`response-ok: ${res.ok}, status: ${res.status}`)
-//       )
-//       .catch((error) => console.log(error));
-//   });
-
-//   //start cron job
-//   cronJob.start();
-
-//   app.listen(process.env.PORT, () => {
-//     console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
-//   });
-// }
-
-// main()
-
 import 'dotenv-safe/config'
+import "reflect-metadata";
+
+import express from "express";
+
+const cors = require("cors");
+const morgan = require('morgan')
+
+// const message = require('./routes/message')
+
 import { client, socketModeClient } from './utils/slack'
 
+import cron from "cron";
+import fetch from 'node-fetch'
 
-//listen for messages 
-const main = async () => {
+const main = () => {
+  const app = express();
+
+  app.use(morgan("dev"));
+
+  app.use(cors({ origin: "*" }));
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get("/", (_, res) => {
+    res.send("Hello world");
+  });
+
+  // app.use('/api/v1/message', message)
+
+  app.use((_, res) => {
+    res.status(404).json({ status: "404" });
+  });
+
+  //listen for messages 
   socketModeClient.on('message', async ({ event }) => {
     await client.reactions.add({
       timestamp: event.ts,
@@ -83,9 +44,48 @@ const main = async () => {
   });
 
   //start listening for messages
-  await socketModeClient.start(process.env.PORT).then(() => {
+  (async () => {
+    await socketModeClient.start();
+  })();
+
+  //prevent heroku from sleeping
+  const cronJob = new cron.CronJob("0 */25 * * * *", () => {
+    fetch("https://clava-slack-app.herokuapp.com/")
+      .then((res) =>
+        console.log(`response-ok: ${res.ok}, status: ${res.status}`)
+      )
+      .catch((error) => console.log(error));
+  });
+
+  //start cron job
+  cronJob.start();
+
+  app.listen(process.env.PORT, () => {
     console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
   });
 }
 
 main()
+
+// import 'dotenv-safe/config'
+// import { client, socketModeClient } from './utils/slack'
+
+
+// //listen for messages 
+// const main = async () => {
+//   socketModeClient.on('message', async ({ event }) => {
+//     await client.reactions.add({
+//       timestamp: event.ts,
+//       name: 'clava',
+//       channel: event.channel
+//     })
+//     console.log('Reaction added')
+//   });
+
+//   //start listening for messages
+//   await socketModeClient.start(process.env.PORT).then(() => {
+//     console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
+//   });
+// }
+
+// main()
