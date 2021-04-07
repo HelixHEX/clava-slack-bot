94704b657ae9d13cdef38b59e7a47c60fddbf6c5
HelixHEX <e.wambugu192@gmail.com>
2021-04-07 13:59:11 -0400

    Added express back

--- src/server.js
+++ src/server.js
@@ -1,33 +1,68 @@
 import 'dotenv-safe/config'
+import "reflect-metadata";
+
+import express from "express";
+
+const cors = require("cors");
+const morgan = require('morgan')
+
+// const message = require('./routes/message')
 
 import { client, socketModeClient } from './utils/slack'
 
 import cron from "cron";
 import fetch from 'node-fetch'
 
-//listen for messages 
-socketModeClient.on('message', async ({ event }) => {
-  await client.reactions.add({
-    timestamp: event.ts,
-    name: 'thumbsdown',
-    channel: event.channel
-  })
-  console.log('Reaction added')
-});
+const main = () => {
+  const app = express();
 
-//start listening for messages
-(async () => {
-  await socketModeClient.start();
-})();
+  app.use(morgan("dev"));
 
-//prevent heroku from sleeping
-const cronJob = new cron.CronJob("0 */25 * * * *", () => {
-  fetch("https://clava-slack-app.herokuapp.com/")
-    .then((res) =>
-      console.log(`response-ok: ${res.ok}, status: ${res.status}`)
-    )
-    .catch((error) => console.log(error));
-});
+  app.use(cors({ origin: "*" }));
 
-//start cron job
-cronJob.start();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get("/", (_, res) => {
+    res.send("Hello world");
+  });
+
+  // app.use('/api/v1/message', message)
+
+  app.use((_, res) => {
+    res.status(404).json({ status: "404" });
+  });
+
+  //listen for messages 
+  socketModeClient.on('message', async ({ event }) => {
+    await client.reactions.add({
+      timestamp: event.ts,
+      name: 'thumbsdown',
+      channel: event.channel
+    })
+    console.log('Reaction added')
+  });
+
+  //start listening for messages
+  (async () => {
+    await socketModeClient.start();
+  })();
+
+  //prevent heroku from sleeping
+  const cronJob = new cron.CronJob("0 */25 * * * *", () => {
+    fetch("https://clava-slack-app.herokuapp.com/")
+      .then((res) =>
+        console.log(`response-ok: ${res.ok}, status: ${res.status}`)
+      )
+      .catch((error) => console.log(error));
+  });
+
+  //start cron job
+  cronJob.start();
+
+  app.listen(process.env.PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
+  });
+}
+
+main()
